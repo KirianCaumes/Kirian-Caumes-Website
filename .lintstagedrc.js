85633fb7c0537ea098bc626727ca7c4a9556f953
KirianCaumes <kirian.caumes@gmail.com>
2023-12-07 20:04:32 +0100

    ⬆️ Upgrade dependencies

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,8 +3,16 @@
  * @type {import('lint-staged').Config}
  */
 module.exports = {
-    '*': ['cspell --no-progress --dot --gitignore'],
-    '*.{ts,tsx,js,jsx}': ['prettier --ignore-unknown --check', 'eslint --max-warnings=0', 'cspell --no-progress --dot --gitignore'],
-    '*.{css,scss,sass}': ['prettier --ignore-unknown --check', 'stylelint --max-warnings 0', 'cspell --no-progress --dot --gitignore'],
+    '*': ['cspell --no-progress --dot --gitignore --no-must-find-files'],
+    '*.{ts,tsx,js,jsx}': [
+        'prettier --ignore-unknown --check',
+        'eslint --max-warnings=0',
+        'cspell --no-progress --dot --gitignore --no-must-find-files',
+    ],
+    '*.{css,scss,sass}': [
+        'prettier --ignore-unknown --check',
+        'stylelint --max-warnings 0',
+        'cspell --no-progress --dot --gitignore --no-must-find-files',
+    ],
     '*.{html,json,svg,yml,xml}': ['prettier --ignore-unknown --check', 'cspell --no-progress --dot --gitignore'],
 }
